354ee23bb2fb73c0da989d81fccb2b366912e069
evb <enbotnaru@gmail.com>
2022-05-20 14:43:09 -0700

    python/currency-exchange/exchange.py

--- python/currency-exchange/exchange.py
+++ python/currency-exchange/exchange.py
@@ -45,7 +45,7 @@ def get_number_of_bills(budget, denomination):
 def exchangeable_value(budget, exchange_rate, spread, denomination):
     """
 
-    :param budget: float - the amount of your money you are planning to exchange.
+    :param budget: float - amount of  money you are planning to exchange.
     :param exchange_rate: float - the unit value of the foreign currency.
     :param spread: int - percentage that is taken as an exchange fee.
     :param denomination: int - the value of a single bill.
@@ -60,13 +60,14 @@ def exchangeable_value(budget, exchange_rate, spread, denomination):
 def non_exchangeable_value(budget, exchange_rate, spread, denomination):
     """
 
-    :param budget: float - the amount of your money you are planning to exchange.
+    :param budget: float - amount of money you are planning to exchange.
     :param exchange_rate: float - the unit value of the foreign currency.
     :param spread: int - percentage that is taken as an exchange fee.
     :param denomination: int - the value of a single bill.
     :return: int non-exchangeable value.
     """
-    real_rate = exchange_rate * (1 + spread / 100)
+    rate = exchange_rate * (1 + spread / 100)
+
     return int(exchange_money(
-        budget, real_rate) - exchangeable_value(
+        budget, rate) - exchangeable_value(
             budget, exchange_rate, spread, denomination))
