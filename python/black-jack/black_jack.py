70b914d1b7b1e4fd348abf5e46251541bc21a0a0
evb <enbotnaru@gmail.com>
2022-05-22 14:08:13 -0700

    python/black-jack/black_jack.py

--- python/black-jack/black_jack.py
+++ python/black-jack/black_jack.py
@@ -49,7 +49,7 @@ def value_of_ace(card_one, card_two):
     2.  'A' (ace card) = 11 (if already in hand)
     3.  '2' - '10' = numerical value.
     """
-    if card_one == "A" or card_two == "A":
+    if "A" in [card_one, card_two]:
         return 1
 
     total_value = value_of_card(card_one) + value_of_card(card_two)
