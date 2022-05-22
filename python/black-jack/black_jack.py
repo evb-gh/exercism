c3ecb8702997c6a282f55a649affa9f892cf7d50
evb <enbotnaru@gmail.com>
2022-05-22 12:13:41 -0700

    python/black-jack/black_jack.py

--- python/black-jack/black_jack.py
+++ python/black-jack/black_jack.py
@@ -49,6 +49,8 @@ def value_of_ace(card_one, card_two):
     2.  'A' (ace card) = 11 (if already in hand)
     3.  '2' - '10' = numerical value.
     """
+    total_value = value_of_card(card_one) + value_of_card(card_two)
+    return 1 if total_value + 11 >= 21 else 11
 
     pass
 
