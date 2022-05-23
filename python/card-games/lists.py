67b1ab81e4042e008d292826b8802ff9825a9a8c
evb <enbotnaru@gmail.com>
2022-05-23 13:51:58 -0700

    python/card-games/lists.py

--- python/card-games/lists.py
+++ python/card-games/lists.py
@@ -68,7 +68,7 @@ def approx_average_is_average(hand):
     if middle % 2 != 0:
         return hand[int(middle - 0.5)] == card_average([hand[0], hand[-1]])
     else:
-        return hand[int(middle)] == card_average(hand)
+        return hand[int(middle)] == card_average([hand[0], hand[-1]])
 
 
 def average_even_is_average_odd(hand):
